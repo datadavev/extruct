068028de5611756b31f2241db531900322a0f780
Dave Vieglais <datadavev@users.noreply.github.com>
2020-04-06 12:47:27 -0400

    removing import of rdfa for rfdlib 5.x

--- extruct/__init__.py
+++ extruct/__init__.py
@@ -1,6 +1,5 @@
 from ._extruct import SYNTAXES, extract
 from .jsonld import JsonLdExtractor
-from .rdfa import RDFaExtractor
 from .w3cmicrodata import MicrodataExtractor
 from .opengraph import OpenGraphExtractor
 from .microformat import MicroformatExtractor
